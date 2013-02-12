4e25969f20c6bf135580f1463ab4c70edf41a990
aposorovaschi <alin.posorovaschi@3pillarglobal.com>
2013-02-12 17:09:30 +0200

    Add methods to illustrate the use of method query creation, and tests to illlustrate their usage.

--- jpa/spring-data-jpa/src/main/java/com/tpg/tmjug/springdata/demo/jpa/repository/PaginAndSortinCustomerRepository.java
+++ jpa/spring-data-jpa/src/main/java/com/tpg/tmjug/springdata/demo/jpa/repository/PaginAndSortinCustomerRepository.java
@@ -1,8 +1,20 @@
 package com.tpg.tmjug.springdata.demo.jpa.repository;
 
 import com.tpg.tmjug.springdata.demo.jpa.entities.Customer;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
 
 public interface PaginAndSortinCustomerRepository extends PagingAndSortingRepository<Customer, Long> {
 
+    @Query("select c from Customer c where c.age >= ?1 and c.age <= ?2")
+    List<Customer> findByAge(int start, int end);
+
+
+    List<Customer> findByAgeGreaterThan(int start);
+
+    List<Customer> findByAgeBetween(int start, int end);
+
+    List<Customer> findByAgeAndName(int age, String name);
 }
--- jpa/spring-data-jpa/src/test/java/com/tpg/tmjug/springdata/demo/jpa/repository/PagingAndSortingCustomerRepositoryTest.java
+++ jpa/spring-data-jpa/src/test/java/com/tpg/tmjug/springdata/demo/jpa/repository/PagingAndSortingCustomerRepositoryTest.java
@@ -2,6 +2,7 @@ package com.tpg.tmjug.springdata.demo.jpa.repository;
 
 import com.tpg.tmjug.springdata.demo.jpa.entities.Address;
 import com.tpg.tmjug.springdata.demo.jpa.entities.Customer;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +18,20 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:META-INF/spring/application-context.xml")
 public class PagingAndSortingCustomerRepositoryTest {
 
+    public static final String DUMMY_CUSTOMER_NAME = "Doe";
     @Autowired
     PaginAndSortinCustomerRepository paginAndSortinCustomerRepository;
+
+    @After
+    public void cleanUp() {
+        paginAndSortinCustomerRepository.deleteAll();
+    }
 
     @Test
     public void should_save_and_retrieve_persisted_customer() {
@@ -41,11 +49,84 @@ public class PagingAndSortingCustomerRepositoryTest {
         assertEquals(customers.get(0), customerPage.getContent().get(0));
     }
 
+    @Test
+    public void findByAttributeAndValue_should_retrieve_the_customers_filtered_by_age() {
+        // Prepare
+        int numberOfDummyCustomers = 9;
+        List<Customer> customers = createDummyCustomers(numberOfDummyCustomers);
+
+        // Exercise
+        List<Customer> filteredCustomers = paginAndSortinCustomerRepository.findByAge(1, numberOfDummyCustomers);
+
+
+        // Verify
+        assertNotNull(filteredCustomers);
+        assertEquals(numberOfDummyCustomers - 1, filteredCustomers.size());
+        assertTrue("The list of customers is filtered by age", customers.containsAll(filteredCustomers));
+
+    }
+
+    @Test
+    public void findByAttributeAndValue_should_retrieve_the_customers_starting_with_age() {
+        // Prepare
+        int numberOfDummyCustomers = 9;
+        List<Customer> customers = createDummyCustomers(numberOfDummyCustomers);
+
+        // Exercise
+        List<Customer> filteredCustomers = paginAndSortinCustomerRepository.findByAgeGreaterThan(2);
+
+
+        // Verify
+        assertNotNull(filteredCustomers);
+        assertEquals(numberOfDummyCustomers - 3, filteredCustomers.size());
+        assertTrue("The list of customers is filtered by age", customers.containsAll(filteredCustomers));
+
+    }
+
+    @Test
+    public void findByAttributeAndValue_should_retrieve_the_customers_between_specific_age_values() {
+        // Prepare
+        int numberOfDummyCustomers = 9;
+        List<Customer> customers = createDummyCustomers(numberOfDummyCustomers);
+
+        // Exercise
+        List<Customer> filteredCustomers = paginAndSortinCustomerRepository.findByAgeBetween(1, numberOfDummyCustomers);
+
+
+        // Verify
+        assertNotNull(filteredCustomers);
+        assertEquals(numberOfDummyCustomers - 1, filteredCustomers.size());
+        assertTrue("The list of customers is filtered by age", customers.containsAll(filteredCustomers));
+
+    }
+
+    @Test
+    public void findByAttributeAndValue_should_retrieve_the_customers_with_specific_age_and_name() {
+        // Prepare
+        int numberOfDummyCustomers = 9;
+        List<Customer> customers = createDummyCustomers(numberOfDummyCustomers);
+
+        // Exercise
+        int age = 1;
+        List<Customer> filteredCustomers = paginAndSortinCustomerRepository.findByAgeAndName(age, DUMMY_CUSTOMER_NAME);
+
+
+        // Verify
+        assertNotNull(filteredCustomers);
+        assertEquals(1, filteredCustomers.size());
+        Customer foundCustomer = filteredCustomers.get(0);
+        assertEquals(DUMMY_CUSTOMER_NAME, foundCustomer.getName());
+        assertEquals(age, foundCustomer.getAge());
+
+    }
+
+
+
     private List<Customer> createDummyCustomers(int numberOfDummyCustomers) {
         List<Customer> customers = new ArrayList<>();
 
-        for(int i = 0; i < numberOfDummyCustomers; i++) {
-            Customer customer = new Customer(new Long(i), "Doe", 21, new Address(new Long(i)));
+        for (int i = 0; i < numberOfDummyCustomers; i++) {
+            Customer customer = new Customer(new Long(i), DUMMY_CUSTOMER_NAME, i, new Address(new Long(i)));
             Customer savedCustomer = paginAndSortinCustomerRepository.save(customer);
             customers.add(customer);
         }
