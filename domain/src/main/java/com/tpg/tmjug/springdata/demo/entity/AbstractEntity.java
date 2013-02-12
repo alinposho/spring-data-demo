1a2b286c92fda90a88df4aa30176c72d890b4a37
aposorovaschi <alin.posorovaschi@3pillarglobal.com>
2013-02-12 11:59:14 +0200

    Changes to use the Spring @Id Annotations.

--- domain/src/main/java/com/tpg/tmjug/springdata/demo/entity/AbstractEntity.java
+++ domain/src/main/java/com/tpg/tmjug/springdata/demo/entity/AbstractEntity.java
@@ -1,47 +1,51 @@
 package com.tpg.tmjug.springdata.demo.entity;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import org.springframework.data.annotation.Id;
 
 public abstract class AbstractEntity {
 
-	@Id
-	private Long id;
-	
-	public AbstractEntity(final Long id) {
-		this.id = id;
-	}
-    public AbstractEntity() { }
-	
-	public Long getId() {
-		return id;
-	}
+    @Id
+    private Long id;
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+    public AbstractEntity(final Long id) {
+        this.id = id;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		AbstractEntity other = (AbstractEntity) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
+    public AbstractEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AbstractEntity other = (AbstractEntity) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
 }
